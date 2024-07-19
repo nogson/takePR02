42e9d03cc452d00b846f73520c908b818dbcd23d
nogson <nogson@users.noreply.github.com>
2024-07-19 05:25:17 +0000

    feat: update variables

---
+++ ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,15 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly
+// Generated on Fri, 19 Jul 2024 05:25:17 GMT
+
+
+import UIKit
+
+internal struct  {
+    internal static let colorColorDark = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    internal static let colorColorLight = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+}
